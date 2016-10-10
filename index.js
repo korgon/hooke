32bb305de6bf730a08f544ac0a1f679792399de0
kevin <korgon@gmail.com>
2016-10-10 01:43:32 -0600

    Got nowhere with this...

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-var options = {
-	port: 9000,
-	loader_path: __dirname,
-	koa_path: __dirname,
-	script_location: '/hooke/hooke.js',
-	debug: false
-}
-
 var hooker = require('./src/hooker.js');
-hooker.init(options);
 
 // Testing
 var url = 'https://www.google.com';
 
-hooker.proxy(url);
+//hooker.proxy(url);
--- src/hooker.js
+++ src/hooker.js
@@ -1,160 +1,94 @@
-// used for proxy in springboard
 'use strict';
 
-var logit = require('./logit.js');
-var hoxy = require('hoxy');
-var http = require('http');
+let http = require('http');
+let https = require('https');
+let connect = require('connect');
+let httpProxy = require('http-proxy');
+let harmon = require('harmon');
+let fs = require('fs');
 
-var proxy;
-var options;
+let logit = require('./logit.js');
+
+let ssl = {
+	key: fs.readFileSync('./ssl/server.key'),
+	cert: fs.readFileSync('./ssl/server.crt')
+}
+
 
 var hooke = function() {
 	var self = this;
+	self.resource_dir = __dirname + '/resources';
 
-	self.init = function(opts) {
-		options = opts;
+	var selects = [];
+	var simpleselect = {};
+
+	simpleselect.query = '.b';
+	simpleselect.func = function(node) {
+			node.createWriteStream().end('<div>+ Trumpet</div>');
 	}
 
-	self.proxy = function(url) {
-		options.url = url;
-		return new Promise(function(resolve, reject) {
-			return self.stop().then(() => {
-				createNewProxy(url, function() {
-					resolve();
-				});
-			}).catch(err => {
-				reject(err);
-			});
+	selects.push(simpleselect);
+
+	selects.push({
+		query: '.a',
+		func: function(node) {
+
+		}
+	});
+
+	//
+	// Basic Connect App
+	//
+	var app = connect();
+
+	var proxy = httpProxy.createServer({
+		ssl,
+		target: 'https://www.google.com',
+		changeOrigin: true,
+		secure: true
+	});
+
+	// Listen for the `error` event on `proxy`.
+	proxy.on('error', function (err, req, res) {
+		res.writeHead(500, {
+			'Content-Type': 'text/plain'
 		});
+		console.log(err);
+
+		res.end('Something went wrong. And we are reporting a custom error message.');
+	});
+
+	//app.use(harmon([], selects, true));
+
+	app.use(function(req, res) {
+			proxy.web(req, res);
+	});
+
+	http.createServer(app).listen(8000);
+
+	https.createServer({
+		key: fs.readFileSync('./ssl/server.key'),
+		cert: fs.readFileSync('./ssl/server.crt'),
+		ca: fs.readFileSync('./ssl/ca.crt')
+	},function(req, res) {
+			res.writeHead(200, {
+					'Content-Type': 'text/html'
+			});
+			res.write('<html><head></head><body><div class="a">Nodejitsu Http Proxy</div><div class="b">&amp; Frames</div></body></html>');
+			res.end();
+	}).listen(9000);
+
+	self.init = function(opts) {
+	}
+
+	self.start = function(site) {
+	}
+
+	self.create = function(settings) {
 	}
 
 	self.stop = function() {
-		return new Promise(function(resolve, reject) {
-			if (proxy && proxy.close) {
-				proxy.close(function() {
-					logit.log('Proxy Stopped', options.proxy_host + ' @ ' + 'http://localhost:' + options.port, 'gray');
-					resolve();
-				});
-			} else {
-				resolve();
-			}
-		});
 	}
 };
-
-function createNewProxy(url, callback) {
-	options.proxy_host = url.replace(/\/$/, '');
-	options.hostname = options.proxy_host.replace(/^http(s)*:\/\//i, '').replace(/^www\./i, '');
-	logit.log('Proxy Started', options.proxy_host + ' @ ' + 'http://localhost:' + options.port, 'gray');
-
-	proxy = hoxy.createServer({
-		reverse: options.proxy_host
-	}).listen(options.port, function() {
-		attachIntercepts();
-
-		callback();
-	});
-}
-
-function attachIntercepts() {
-	proxy.log('error warn', function(event) {
-		logit.log('Error', '', 'fail');
-		console.error(event.level + ': ' + event.message);
-		if (event.error) console.error(event.error.stack);
-	});
-
-	// Intercept Response
-	proxy.intercept({
-		phase: 'response',
-		mimeType: 'text/html',
-		as: '$'
-	}, function(req, resp, cycle) {
-
-		if(options.debug) logit.log('Response Intercept', '', 'warn');
-		if(options.debug) logit.log('Request', '', 'none');
-		if(options.debug) console.log(req);
-
-		if(options.debug) logit.log('Original Response', '', 'none');
-		if(options.debug) console.log(resp);
-
-		resp.headers['cache-control'] = 'max-age=1';
-
-		if (resp.statusCode == 301 || resp.statusCode == 302) {
-			if(options.debug) logit.log('Proxy Redirect', 'Encountered a redirect... ', 'red');
-
-			resp.headers.location = '/badhooke?req=' + encodeURIComponent(options.url) + '&resp=' + encodeURIComponent(resp.headers.location);
-		}
-
-		// modify things
-		resp.$('title').text('Hooke Proxy');
-		resp.$('head').prepend('<script type="text/javascript" src="' + options.script_location + '">');
-
-		// make hrefs proxy friendly
-		resp.$('a').each(function(i, anchor) {
-			let regex = new RegExp('^.*' + options.hostname, 'g');
-			let href = resp.$(anchor).attr('href');
-			if (href) {
-				href = href.replace(regex, '');
-				resp.$(anchor).attr('href', href);
-			}
-		});
-		// do the same for forms
-		resp.$('form').each(function(i, form) {
-			let regex = new RegExp('^.*' + options.hostname, 'g');
-			let action = resp.$(form).attr('action');
-			if (action) {
-				action = action.replace(regex, '');
-				resp.$(form).attr('action', action);
-			}
-		});
-
-		// remove any searchspring script tags
-		resp.$('script').each(function(i, script) {
-			let src = resp.$(script).attr('src');
-			if (src && src.match(/searchspring\.net/i)) {
-				resp.$(script).remove();
-			}
-		});
-
-		// view the response
-		if(options.debug) logit.log('Modified Response', '', 'none');
-		if(options.debug) console.log(resp);
-
-	});
-
-	// Serve up invalid.html
-	proxy.intercept({
-		phase: 'request',
-		fullUrl: options.proxy_host + '/badhooke'
-	}, function(req, resp, cycle) {
-		if(options.debug) logit.log('Hooke', 'serving invalid.html (' + options.koa_path + '/resources/invalid.html)', 'green');
-		return cycle.serve({
-			path: options.koa_path + '/resources/invalid.html'
-		});
-	});
-
-	// Serve up hooke.js
-	proxy.intercept({
-		phase: 'request',
-		fullUrl: options.proxy_host + '/hooke/hooke.js'
-	}, function(req, resp, cycle) {
-		if(options.debug) logit.log('Hooke', 'serving hooke.js (' + options.loader_path + '/resources/hooke.js)', 'green');
-		return cycle.serve({
-			path: options.loader_path + '/resources/hooke.js'
-		});
-	});
-
-	// Serve up hooke.css
-	proxy.intercept({
-		phase: 'request',
-		fullUrl: options.proxy_host + '/hooke/hooke.css'
-	}, function(req, resp, cycle) {
-		if(options.debug) logit.log('Hooke', 'serving hooke.css', 'green');
-		if(options.debug) console.log(rootdir + '/resources/hooke.css');
-		return cycle.serve({
-			path: rootdir + '/resources/hooke.css'
-		});
-	});
-}
 
 module.exports = new hooke;
